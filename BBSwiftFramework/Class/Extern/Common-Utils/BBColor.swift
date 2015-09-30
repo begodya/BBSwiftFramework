2af91c0ee27132be3428d0e697e8b981cb7f9c40
hmily-xinxiang@163.com <you@example.com>
2015-09-30 17:08:04 +0800

    BBColor

--- BBSwiftFramework/Class/Extern/Common-Utils/BBColor.swift
+++ BBSwiftFramework/Class/Extern/Common-Utils/BBColor.swift
@@ -10,7 +10,38 @@ import UIKit
 
 class BBColor: NSObject {
 
+    // MARK: - 颜色接口
     func titleColor() -> UIColor {
-        return UIColor.blackColor()
+        return colorWithHexString("000000", alpha: 1)
+    }
+
+    func detailColor() -> UIColor {
+        return colorWithHexString("646464", alpha: 1)
+    }
+    
+    
+    
+    // MARK: - Creates a UIColor from a Hex string.
+    private func colorWithHexString (hex: String, alpha: CGFloat) -> UIColor {
+        var cString: String = hex.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()).uppercaseString
+        
+        if (cString.hasPrefix("#")) {
+            cString = (cString as NSString).substringFromIndex(1)
+        }
+        
+//        if (count(cString) != 6) {
+//            return UIColor.grayColor()
+//        }
+        
+        let rString = (cString as NSString).substringToIndex(2)
+        let gString = ((cString as NSString).substringFromIndex(2) as NSString).substringToIndex(2)
+        let bString = ((cString as NSString).substringFromIndex(4) as NSString).substringToIndex(2)
+        
+        var r:CUnsignedInt = 0, g:CUnsignedInt = 0, b:CUnsignedInt = 0
+        NSScanner(string: rString).scanHexInt(&r)
+        NSScanner(string: gString).scanHexInt(&g)
+        NSScanner(string: bString).scanHexInt(&b)
+        
+        return UIColor(red: CGFloat(r) / 255.0, green: CGFloat(g) / 255.0, blue: CGFloat(b) / 255.0, alpha: alpha)
     }
 }
