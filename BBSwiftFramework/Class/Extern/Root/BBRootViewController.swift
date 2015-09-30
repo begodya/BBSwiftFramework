1d0b40a1e8226cf2e99b2696e70f2d9269e230eb
hmily-xinxiang@163.com <you@example.com>
2015-09-30 12:57:08 +0800

    BBSwiftFramework

---
+++ BBSwiftFramework/Class/Extern/Root/BBRootViewController.swift
@@ -0,0 +1,48 @@
+//
+//  BBRootViewController.swift
+//  BBSwiftFramework
+//
+//  Created by Bei Wang on 9/30/15.
+//  Copyright © 2015 Bei. All rights reserved.
+//
+
+import UIKit
+
+class BBRootViewController: UIViewController {
+    
+    
+    // MARK: - --------------------System--------------------
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - --------------------功能函数--------------------
+    // MARK: 初始化
+    
+    // MARK: - --------------------手势事件--------------------
+    // MARK: 各种手势处理函数注释
+    
+    // MARK: - --------------------按钮事件--------------------
+    // MARK: 按钮点击函数注释
+    
+    // MARK: - --------------------代理方法--------------------
+    // MARK: - 代理种类注释
+    // MARK: 代理函数注释
+    
+    // MARK: - --------------------属性相关--------------------
+    // MARK: 属性操作函数注释
+    
+    // MARK: - --------------------接口API--------------------
+    // MARK: 分块内接口函数注释
+
+
+}
+
+
---
+++ BBSwiftFramework/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BBSwiftFramework
+//
+//  Created by Bei Wang on 9/30/15.
+//  Copyright © 2015 Bei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
