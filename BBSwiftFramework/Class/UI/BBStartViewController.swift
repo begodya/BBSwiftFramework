bb516f2d9898a9e34a4098b64294c5c38f15e328
hmily-xinxiang@163.com <you@example.com>
2015-10-14 13:20:27 +0800

    code

--- BBSwiftFramework/Class/UI/BBStartViewController.swift
+++ BBSwiftFramework/Class/UI/BBStartViewController.swift
@@ -60,14 +60,12 @@ class BBStartViewController: BBRootViewController {
     
     func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
         let cell = tableView .dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as UITableViewCell
-        let row = indexPath.row as Int
-        cell.textLabel!.text = self.items[row]
+        cell.textLabel!.text = self.items[indexPath.row as Int]
         return cell;
     }
 
     func tableView(tableView: UITableView!, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath!) {
-        let index = indexPath.row as Int
-        self.items.removeAtIndex(index)
+        self.items.removeAtIndex(indexPath.row as Int)
         self.contentTableView?.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Top)
         NSLog("删除\(indexPath.row)")
     }
